cb5a93dfc5cab34c67b392c1abbd56b3e2f6f318
JulSarmiento <jasa1999@hotmail.com>
2022-11-07 19:36:30 -0500

    chat con socket.io funccionando

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const {Server: HttpServer} = require('http');
+const {Server: IoServer} = require('socket.io');
 
 const indexRouter = require('./src/routes/index');
 const errorHandler = require('./src/middlewares/error.middleware');
 const pageNotFound = require('./src/middlewares/notfound.middleware');
 const app = express()
+const http = new HttpServer(app);
+const io = new IoServer(http);
+
+const messages = [];
+const producst = [];
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -15,7 +22,17 @@ app.set('views', './views');
 
 app.use(indexRouter);
 
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado.');
+
+  socket.emit('UPDATE_DATA', messages);
+  socket.on('NEW_MESSAGE_TO_SERVER', data => {
+    messages.push(data);
+    io.sockets.emit('NEW_MESSAGE_FROM_SERVER', data)
+  })
+})
+
 app.use(errorHandler);
 app.use(pageNotFound);
 
-module.exports = app;
+module.exports = http;
--- public/main.js
+++ public/main.js
@@ -1,1 +1,48 @@
-alert('holis')
+const socket = io();
+const btn = document.querySelector('#send-btn');
+const list = document.querySelector('#chat-box');
+const messages = [];
+
+function sendNewMessage(){
+  const username = document.querySelector('#username').value;
+  const message = document.querySelector('#message').value;
+
+  if(!message || !username) {
+    return
+  }
+
+  const messageObject = {
+    username,
+    message,
+    timestamp: new Date().toLocaleString()
+  }
+
+  socket.emit('NEW_MESSAGE_TO_SERVER', messageObject);
+  document.querySelector('#message').value = '';
+}
+
+function printMessages(messages) {
+  list.innerHTML = '';
+  if(messages.length  !== 0) {
+    messages.forEach(element => {
+      list.insertAdjacentHTML('beforeend', `<li> <span class="username" >${element.username}</span> (<span class="timestamp">${element.timestamp}</span>): <span class="message-text">${element.message}</span></li>`)
+    });
+  }
+
+}
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  sendNewMessage();
+});
+
+socket.on('UPDATE_DATA', (data) => {
+  console.log('Estoy recibiendo data', data);
+  message = data;
+  printMessages(data);
+})
+
+socket.on('NEW_MESSAGE_FROM_SERVER', (data) => {
+  messages.push(data);
+  printMessages(messages)
+})
